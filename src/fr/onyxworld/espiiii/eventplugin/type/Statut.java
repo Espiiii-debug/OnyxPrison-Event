011f82f4fecfff4f25d42bec928679c8c87a6f91
Espiiii-debug <59062281+Espiiii-debug@users.noreply.github.com>
2021-05-11 20:52:34 +0200

    Re-Importation du plugin

---
+++ src/fr/onyxworld/espiiii/eventplugin/type/Statut.java
@@ -0,0 +1,28 @@
+package fr.onyxworld.espiiii.eventplugin.type;
+
+import java.util.ArrayList;
+
+public class Statut 
+{
+	
+	// Type d'event
+	public int type;
+	
+	// Nombre de joueurs ayant participé
+	public int numberOfPlayer;
+	
+	// Arguments supplémentaires
+	public String additionalArgs;
+		
+	public Statut(int type, int numberOfPlayer, String additionalArgs)
+	{
+		
+		this.type = type;
+		this.numberOfPlayer = numberOfPlayer;
+		this.additionalArgs = additionalArgs;
+		
+	}
+	
+	
+	
+}
